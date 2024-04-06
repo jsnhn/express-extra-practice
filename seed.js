a417a7eeaa9985d5faeda5d3ebcc10f14b40cbc6
jsnhn <jayhh122@gmail.com>
2024-04-05 20:01:52 -0700

    updated seed.js

--- seed.js
+++ seed.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 require('./config/database');
 
 const Skill = require('./models/skill');
+const PlayerProfile = require('./models/player-profile')
 
 const data = require('./data');
 
@@ -11,6 +12,31 @@ const data = require('./data');
 
     results = await Skill.create(data.skills)
     console.log(results)
+
+    try {
+        const cursor = await PlayerProfile.aggregate([
+            {
+                $lookup:
+                    {
+                        from: 'skills',
+                        localField: '_id',
+                        foreignField: 'playerprofile',
+                        as: 'matched_docs'
+                    }
+            },
+            {
+                $match: { 'matched_docs': { $eq: [] }}
+            }
+        ]);
+    
+        const playerprofile_ids = await cursor.map(function (doc) { return doc._id});
+        await PlayerProfile.deleteMany({'_id': { '$in': playerprofile_ids}})
+
+        console.log(results);
+    } catch (error) {
+       
+        console.error(err);
+    }
 
     process.exit();
 })();
