fa0cdd265476a545800dac90050209dc8856c87d
jsnhn <jayhh122@gmail.com>
2024-04-09 21:05:13 -0700

    updated seed.js

--- seed.js
+++ seed.js
@@ -7,36 +7,53 @@ const PlayerProfile = require('./models/player-profile')
 const data = require('./data');
 
 (async function() {
-    let results = await Skill.deleteMany({})
-    console.log(results)
+    const deletedSkills = await Skill.find({})
+    // const result = await Skill.deleteMany({})
+    // console.log(result)
+    console.log('deletedSkills: ', deletedSkills)
 
-    results = await Skill.create(data.skills)
-    console.log(results)
+    // const newSkills = await Skill.create(data.skills)
+    // console.log(newSkills)
 
-    try {
-        const cursor = await PlayerProfile.aggregate([
-            {
-                $lookup:
-                    {
-                        from: 'skills',
-                        localField: '_id',
-                        foreignField: 'playerprofile',
-                        as: 'matched_docs'
-                    }
-            },
-            {
-                $match: { 'matched_docs': { $eq: [] }}
-            }
-        ]);
+    const playerProfiles = await PlayerProfile.find({})
+    playerProfiles.forEach(playerProfile => {
+        playerProfile.skills.forEach(skill => {
+            console.log(skill)
+            deletedSkills.find(deletedSkill => deletedSkill.type === skill.type);
+            const newSkill = Skill.find({name: deletedSkills.name})
+            skill.type = newSkill._id
+            playerProfile.save();
+
+            //TODO first use the skill.type to grab the correct skill object from deletedSkills
+            // then grab the new skill whose name matches the deleted skill you just grabbed and store it in a variable
+            // use that new skill's id that i just grabbed to replace the old skill id in the skill.type, the first 2 steps are gonna use the same array method
+        })
+    })
+
+    // try {
+    //     const cursor = await PlayerProfile.aggregate([
+    //         {
+    //             $lookup:
+    //                 {
+    //                     from: 'skills',
+    //                     localField: '_id',
+    //                     foreignField: 'playerprofile',
+    //                     as: 'matched_docs'
+    //                 }
+    //         },
+    //         {
+    //             $match: { 'matched_docs': { $eq: [] }}
+    //         }
+    //     ]);
     
-        const playerprofile_ids = await cursor.map(function (doc) { return doc._id});
-        await PlayerProfile.deleteMany({'_id': { '$in': playerprofile_ids}})
+    //     const playerprofile_ids = await cursor.map(function (doc) { return doc._id});
+    //     await PlayerProfile.deleteMany({'_id': { '$in': playerprofile_ids}})
 
-        console.log(results);
-    } catch (error) {
+    //     console.log(results);
+    // } catch (error) {
        
-        console.error(err);
-    }
+    //     console.error(err);
+    // }
 
     process.exit();
 })();
